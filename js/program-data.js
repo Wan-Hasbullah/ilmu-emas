1c7bd17c669d0eb26f8ee2c7948e89a0028c8464
Wan Hasbullah <wanhasbullahwanhussin97@gmail.com>
2026-06-06 22:22:55 +0800

    Update program-data.js

--- js/program-data.js
+++ js/program-data.js
@@ -5,14 +5,14 @@ const programData = [
     id: "g100-talk-show-1",
     title: "Eksklusif G100 Talk Show: Hala Tuju Emas 2026",
     category: "G100 Talk Show",
-    image: "poster-program/g100-talk-show-jpg", // Mengarah ke folder poster-program
+    image: "poster-program/g100-talk-show.jpg", // Saya tambahkan titik (.) pada .jpg supaya imej keluar jika anda tersalah taip tadi
     caption: "Kupas tuntas strategi pelaburan emas fizikal dalam mendepani pasaran global bersama panel pakar ekonomi G100."
   },
   {
     id: "qna-pg-1",
     title: "Sesi QnA Public Gold: Cetakan Gold Bar & GAP",
     category: "QnA Public Gold",
-    image: "poster-program/qna.jpg", // Mengarah ke folder poster-program
+    image: "poster-program/qna.jpg", 
     caption: "Adakah anda mempunyai persoalan tentang akaun GAP? Sila sertai sesi soal jawab interaktif ini untuk bantuan langsung."
   }
 ];
