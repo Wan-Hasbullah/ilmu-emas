dbb206896e9594292a25be29667b3c8a65ab99ce
Wan Hasbullah <wanhasbullahwanhussin97@gmail.com>
2026-06-06 16:14:06 +0800

    Create petikan-data.js

---
+++ js/petikan-data.js
@@ -0,0 +1,10 @@
+// Data Petikan - Sedia dipanggil secara global oleh fail pengurusan petikan
+const PETIKAN_DATA = [
+  {
+    id: "ilmu-dan-kemahiran-berjaya",
+    author: "Mohd Zulkifli Shafie",
+    category: "Tips Emas", // Boleh ditukar kepada "Tips Kewangan" jika perlu
+    quote: "Kalau tak pandai boleh belajar. Kalau tak mahir boleh berlatih sampai mahir. Banyak perkara dalam hidup tak perlukan bakat untuk berjaya. Tapi ilmu dan kemahiran yang boleh dipelajari. Siapa sanggup belajar dan berusaha, dia pasti akan berjaya. InsyaAllah.",
+    date: "2026-06-06"
+  }
+];
