dc0d8e33bd400f5658f04f0e272f3bfc85a58fa9
Wan Hasbullah <wanhasbullahwanhussin97@gmail.com>
2026-06-07 21:49:21 +0800

    Update artikel-data.js

--- js/artikel-data.js
+++ js/artikel-data.js
@@ -9,3 +9,13 @@ const ARTIKEL_DATA = [
     path: "kandungan-artikel/panduan-simpanan-emas.html"
   }
 ];
+
+{
+    id: "sejarah-harga-emas-10-tahun",
+    title: "Sejarah Harga Emas 10 Tahun",
+    author: "Ainul Haya",
+    category: "Sejarah Harga Emas",
+    summary: "Paras harga emas semasa yang sedang rendah merupakan peluang terbaik untuk pelabur menambah simpanan, bukannya untuk berasa takut atau berhenti menyimpan.",
+    path: "kandungan-artikel/sejarah-harga-emas-10-tahun.html"
+  }
+];
