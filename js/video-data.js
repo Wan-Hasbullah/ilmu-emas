51780040da8a18943bad2520e1c7edd0d5a6d448
Wan Hasbullah <wanhasbullahwanhussin97@gmail.com>
2026-06-08 09:57:56 +0800

    Update video-data.js

--- js/video-data.js
+++ js/video-data.js
@@ -8,11 +8,11 @@ const VIDEO_DATA = [
     embedCode: `<iframe class="w-full aspect-video rounded-xl" src="https://www.youtube.com/embed/VUTRLtedEzo?si=zqKl9sfgvWIYLUS0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
   },
   {
-    id: "dua-masalah-kewangan",
-    title: "Dua Masalah Utama Kewangan & Cara Mengatasinya",
+    id: "bergerak-ke-arah-kekayaan?",
+    title: "Macam mana nak tahu kita sedang bergerak ke arah kekayaan?",
     category: "ilmu-kewangan",
     type: "tiktok",
-    embedCode: `<blockquote class="tiktok-embed mx-auto" cite="https://www.tiktok.com/@mohdzulkifli.com/video/7396660084996115732" data-video-id="7396660084996115732" style="max-width: 100%; min-width: 325px;"><section><a target="_blank" title="@mohdzulkifli.com" href="https://www.tiktok.com/@mohdzulkifli.com?refer=embed">@mohdzulkifli.com</a></section></blockquote><script async src="https://www.tiktok.com/embed.js"></script>`
+    embedCode: <blockquote class="tiktok-embed" cite="https://www.tiktok.com/@mohdzulkifli.com/video/7648484980837846292" data-video-id="7648484980837846292" style="max-width: 605px;min-width: 325px;" > <section> <a target="_blank" title="@mohdzulkifli.com" href="https://www.tiktok.com/@mohdzulkifli.com?refer=embed">@mohdzulkifli.com</a> Macam mana nak tahu kita sedang bergerak ke arah kekayaan? <a title="membinasatujutapertama" target="_blank" href="https://www.tiktok.com/tag/membinasatujutapertama?refer=embed">#membinasatujutapertama</a> <a title="menabung" target="_blank" href="https://www.tiktok.com/tag/menabung?refer=embed">#menabung</a> <a title="mindset" target="_blank" href="https://www.tiktok.com/tag/mindset?refer=embed">#mindset</a> <a title="misiubahhidup1000keluarga" target="_blank" href="https://www.tiktok.com/tag/misiubahhidup1000keluarga?refer=embed">#misiubahhidup1000keluarga</a> <a title="cashflow" target="_blank" href="https://www.tiktok.com/tag/cashflow?refer=embed">#cashflow</a> <a target="_blank" title="♬ original sound  - Mohd Zulkifli Shafie" href="https://www.tiktok.com/music/original-sound-Mohd-Zulkifli-Shafie-7648485026128022273?refer=embed">♬ original sound  - Mohd Zulkifli Shafie</a> </section> </blockquote> <script async src="https://www.tiktok.com/embed.js"></script>`
   },
   {
     id: "ciri-emas-paling-untung",
