c996b7a19868226e3658b175e78efb51cc2220d0
Wan Hasbullah <wanhasbullahwanhussin97@gmail.com>
2026-06-13 04:03:53 +0800

    Update program-data.js

--- js/program-data.js
+++ js/program-data.js
@@ -3,12 +3,12 @@
 const programData = [
   
   {
-    id: "m1jp-pg-1",
-    title: "Membina Rm1 Juta Pertama",
-    category: "M1JP",
-    image: "poster-program/m1jp.jpg", 
-    caption: "Siapa nak belajar tingkatkan jumlah TABUNGAN, selesaikan masalah HUTANG dan bina HARTA sampai RM 1 Juta Pertama, jemput join ke Private Webinar Membina Satu Juta Pertama.",
-    link: "https://wasap.my/601126704897/M1JP",
-    date: "2026-06-12T20:30:00" // Tarikh & Masa Program (Format ISO)
+    id: "kkde-pg-1",
+    title: "Karnival Kaya Dengan Emas",
+    category: "KKDE",
+    image: "poster-program/karnival-kaya-dengan-emas.jpg",
+    caption: "💰 Ramai orang bekerja keras setiap bulan. Tetapi tidak ramai yang tahu cara menyimpan kekayaan dengan betul.\n\nKalau anda nak kenal dunia emas dengan lebih dekat, jangan lepaskan peluang ini.\n\nPelbagai aktiviti menarik menanti :\n\n💎 Cuci & Uji Emas Percuma 💰 Jual Emas Pada Harga Tinggi 🏧 Gold ATM 🎤 Seminar Emas 🎁 Cabutan Bertuah 🎈 Kids Corner\n\n⚠️ Sekiranya anda berada di sekitar KL, rugi kalau tak singgah. Maklumat acara:-\n\n📅 28 Jun 2026 📍 Menara Public Gold, Kuala Lumpur 🎟️ MASUK PERCUMA\n\nBawa keluarga sekali. Datang untuk ilmu, pulang dengan pengalaman yang berharga.\n\nJumpa anda di sana! ✨",
+    link: "https://wasap.my/601126704897/KKDE",
+    date: "2026-06-28T09:00:00" // Tarikh & Masa Mula Program (Format ISO)
   },
 ];
