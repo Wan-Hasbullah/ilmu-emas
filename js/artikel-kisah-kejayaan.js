f6321b4a1958c6f539d69d38a0fb7c38d5f9a366
Wan Hasbullah <wanhasbullahwanhussin97@gmail.com>
2026-06-13 02:30:22 +0800

    Update artikel-kisah-kejayaan.js

--- js/artikel-kisah-kejayaan.js
+++ js/artikel-kisah-kejayaan.js
@@ -2,7 +2,7 @@
 const ARTIKEL_KISAH_KEJAYAAN = [
   {
     id: "mohd-zulkifli-shafie",
-    title: "Kisah Kejayaan : Mohd Zulkifli Shafie",
+    title: "Mohd Zulkifli Shafie",
     author: "Mohd Zulkifli Shafie",
     category: "PG Business Owner",
     summary: "Bermula sekadar simpan emas untuk diri sendiri, saya tak sangka akhirnya Public Gold dapat memberi jaminan kewangan buat isteri dan anak-anak saya.",
@@ -11,7 +11,7 @@ const ARTIKEL_KISAH_KEJAYAAN = [
   
   {
     id: "rahman-ismail",
-    title: "Kisah Kejayaan : Rahman Ismail",
+    title: "Rahman Ismail",
     author: "Rahman Ismail",
     category: "Mahasiswa",
     summary: "Menembus Batas Fizikal: Impian Bersara Sebelum Bekerja Melalui Emas.",
@@ -25,5 +25,14 @@ const ARTIKEL_KISAH_KEJAYAAN = [
     category: "Usahawan",
     summary: "Inspirasi Bagaimana Membina Empayar Pasif Income 5 Angka Melalui Ilmu Emas.",
     path: "kandungan-artikel-kisah-kejayaan/shara-ismail.html"
-  }
+  },
+
+  {
+    id: "hafiz-shafie",
+    title: "Bisnes Emas Public Gold Jalan Terpantas Capai Kebebasan Masa Dan Kewangan",
+    author: "Hafiz Shafie",
+    category: "Jurutera",
+    summary: "Kisah inspirasi bekas jurutera mekanikal yang berjaya membina empayar pendapatan pasif 5 angka sebulan bersama Public Gold dan sistem G100, membolehkan beliau bersara awal demi meluangkan masa sepenuhnya bersama keluarga.",
+    path: "kandungan-artikel-kisah-kejayaan/hafiz-shafie.html"
+  },
 ];
