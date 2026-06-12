1abc921624543cfa4d868981367b67d976b17742
Wan Hasbullah <wanhasbullahwanhussin97@gmail.com>
2026-06-12 16:15:58 +0800

    Update program-data.js

--- js/program-data.js
+++ js/program-data.js
@@ -1,15 +1,7 @@
 // js/program-data.js
 
 const programData = [
-  {
-    id: "g100-talk-show-1",
-    title: "G100 TALKSHOW: BELI TANAH CASH DENGAN EMAS",
-    category: "G100 Talk Show",
-    image: "poster-program/beli-tanah-cash-dengan-emas.jpg",
-    caption: "Siapa nak belajar tingkatkan jumlah TABUNGAN, selesaikan masalah HUTANG, dan bina HARTA hasil simpanan emas, jemput join live G100 Talkshow malam ni.",
-    link: "https://g100.my/talkshow/",
-    date: "2026-06-10T21:00:00" // Tarikh & Masa Program (Format ISO)
-  },
+  
   {
     id: "m1jp-pg-1",
     title: "Membina Rm1 Juta Pertama",
@@ -18,5 +10,5 @@ const programData = [
     caption: "Siapa nak belajar tingkatkan jumlah TABUNGAN, selesaikan masalah HUTANG dan bina HARTA sampai RM 1 Juta Pertama, jemput join ke Private Webinar Membina Satu Juta Pertama.",
     link: "https://wasap.my/601126704897/M1JP",
     date: "2026-06-12T20:30:00" // Tarikh & Masa Program (Format ISO)
-  }
+  },
 ];
