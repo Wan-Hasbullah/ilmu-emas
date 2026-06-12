7a5b3c042a8fd1e6269fa6472922d0f3dec2d514
Wan Hasbullah <wanhasbullahwanhussin97@gmail.com>
2026-06-13 04:08:43 +0800

    Update program-data.js

--- js/program-data.js
+++ js/program-data.js
@@ -6,9 +6,9 @@ const programData = [
     id: "kkde-pg-1",
     title: "Karnival Kaya Dengan Emas",
     category: "KKDE",
-    image: "poster-program/karnival-kaya-dengan-emas.jpg",
+    image: "poster-program/karnival-kaya-dengan-emas.jpg", 
     caption: "💰 Ramai orang bekerja keras setiap bulan. Tetapi tidak ramai yang tahu cara menyimpan kekayaan dengan betul.\n\nKalau anda nak kenal dunia emas dengan lebih dekat, jangan lepaskan peluang ini.\n\nPelbagai aktiviti menarik menanti :\n\n💎 Cuci & Uji Emas Percuma 💰 Jual Emas Pada Harga Tinggi 🏧 Gold ATM 🎤 Seminar Emas 🎁 Cabutan Bertuah 🎈 Kids Corner\n\n⚠️ Sekiranya anda berada di sekitar KL, rugi kalau tak singgah. Maklumat acara:-\n\n📅 28 Jun 2026 📍 Menara Public Gold, Kuala Lumpur 🎟️ MASUK PERCUMA\n\nBawa keluarga sekali. Datang untuk ilmu, pulang dengan pengalaman yang berharga.\n\nJumpa anda di sana! ✨",
-    link: "https://wasap.my/601126704897/KKDE",
+    link: "https://maps.google.com/maps?vet=10CAAQoqAOahcKEwiAw_GuwIKVAxUAAAAAHQAAAAAQCQ..i&pvq=CgsvZy8xdGp2MF9wbCIPCglsaW5rIG1hcHMQAhgD&lqi=ChxsaW5rIG1hcHMgbWVuYXJhIHB1YmxpYyBnb2xkSLL9gY3mgICACFosEAAQARgCGAMYBCIcbGluayBtYXBzIG1lbmFyYSBwdWJsaWMgZ29sZDICZW6SAQZvZmZpY2WaASNDaFpEU1VoTk1HOW5TMFZKUTBGblNVTnVObkJFYzFsM0VBRfoBBAgAEC0&fvr=1&cs=0&um=1&ie=UTF-8&fb=1&gl=my&sa=X&ftid=0x31cc3630545275eb:0xd4556924b6a9fc5c",
     date: "2026-06-28T09:00:00" // Tarikh & Masa Mula Program (Format ISO)
-  },
+}
 ];
