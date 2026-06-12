b85b5e4ea2f1fa40f0a718b1ee94825ba8819c02
Wan Hasbullah <wanhasbullahwanhussin97@gmail.com>
2026-06-12 18:06:08 +0800

    Create artikel-kisah-kejayaan.js

---
+++ js/artikel-kisah-kejayaan.js
@@ -0,0 +1,20 @@
+// Data Artikel Kisah Kejayaan - Sedia dipanggil secara global oleh fail pengurusan artikel
+const ARTIKEL_KISAH_KEJAYAAN = [
+  {
+    id: "kisah-kejayaan-pelabur-muda",
+    title: "Kisah Kejayaan Pelabur Muda: Bebas Hutang Sebelum Usia 30 Tahun dengan Emas",
+    author: "Wan Hasbullah",
+    category: "Kisah Kejayaan",
+    summary: "Inspirasi bagaimana seorang pemuda menguruskan kewangan dan membina kekayaan konsisten melalui strategi simpanan emas yang bijak.",
+    path: "kandungan-artikel-kejayaan/kisah-kejayaan-pelabur-muda.html"
+  },
+  
+  {
+    id: "kisah-suri-rumah-jutawan-emas",
+    title: "Dari Suri Rumah ke Usahawan Emas: Kisah Inspirasi Puan Aminah",
+    author: "Ainul Haya",
+    category: "Kisah Kejayaan",
+    summary: "Bagaimana seorang suri rumah tangga berjaya menjana pendapatan pasif lima angka sebulan hanya bermula dengan modal satu gram emas.",
+    path: "kandungan-artikel-kejayaan/kisah-suri-rumah-jutawan-emas.html"
+  }
+];
