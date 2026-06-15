0cd7f299825dd77c9900d8b535dc3231d2b6c901
Wan Hasbullah <wanhasbullahwanhussin97@gmail.com>
2026-06-15 08:52:53 +0800

    Update program-data.js

--- js/program-data.js
+++ js/program-data.js
@@ -10,5 +10,16 @@ const programData = [
     caption: "💰 Ramai orang bekerja keras setiap bulan. Tetapi tidak ramai yang tahu cara menyimpan kekayaan dengan betul.\n\nKalau anda nak kenal dunia emas dengan lebih dekat, jangan lepaskan peluang ini.\n\nPelbagai aktiviti menarik menanti :\n\n💎 Cuci & Uji Emas Percuma 💰 Jual Emas Pada Harga Tinggi 🏧 Gold ATM 🎤 Seminar Emas 🎁 Cabutan Bertuah 🎈 Kids Corner\n\n⚠️ Sekiranya anda berada di sekitar KL, rugi kalau tak singgah. Maklumat acara:-\n\n📅 28 Jun 2026 📍 Menara Public Gold, Kuala Lumpur 🎟️ MASUK PERCUMA\n\nBawa keluarga sekali. Datang untuk ilmu, pulang dengan pengalaman yang berharga.\n\nJumpa anda di sana! ✨",
     link: "https://maps.google.com/maps?vet=10CAAQoqAOahcKEwiAw_GuwIKVAxUAAAAAHQAAAAAQCQ..i&pvq=CgsvZy8xdGp2MF9wbCIPCglsaW5rIG1hcHMQAhgD&lqi=ChxsaW5rIG1hcHMgbWVuYXJhIHB1YmxpYyBnb2xkSLL9gY3mgICACFosEAAQARgCGAMYBCIcbGluayBtYXBzIG1lbmFyYSBwdWJsaWMgZ29sZDICZW6SAQZvZmZpY2WaASNDaFpEU1VoTk1HOW5TMFZKUTBGblNVTnVObkJFYzFsM0VBRfoBBAgAEC0&fvr=1&cs=0&um=1&ie=UTF-8&fb=1&gl=my&sa=X&ftid=0x31cc3630545275eb:0xd4556924b6a9fc5c",
     date: "2026-06-28T09:00:00" // Tarikh & Masa Mula Program (Format ISO)
-}
+},
+
+  {
+    {
+    id: "wkde-pg-1",
+    title: "Workshop Kaya Dengan Emas",
+    category: "WKDE", // Ditukar kepada huruf besar untuk konsistensi dengan KKDE
+    image: "poster-program/workshop-kaya-dengan-emas.jpg", // Sila tukar nama fail mengikut poster sebenar anda
+    caption: "Alang-alang simpan emas, biar sampai jadi kaya.\n\nSiapa yang nak belajar secara bersemuka, teknik sebenar \"kaya dengan emas\";\n\n✅ Capai target TABUNGAN 12 bulan gaji\n✅ Selesaikan masalah HUTANG\n✅ Kembangkan HARTA (rumah, tanah, bisnes dsb).\n\nJemput datang belajar di dalam Workshop Kaya Dengan Emas ni.\n\n🗓️ 17 Jun 2026 (Rabu)\n⏰ 8.00 - 10.00 malam\n📍 G100 Academy, Level 7, Menara Public Gold @ TRX, KL\n\n*Yuran RM29.00*.\nSeat terhad kepada 30 peserta saja.\n\n*NOTA: Workshop ini eksklusif untuk penyimpan emas berdaftar (sudah ada PG Code) di bawah dealer aktif G100 Network & 5G Associates saja.*",
+    link: "https://pg2u.my/app/workshop/reg/3/wanhasbullah",
+    date: "2026-06-17T20:00:00" // Tarikh & Masa Mula Program (8.00 Malam format ISO)
+  }
 ];
