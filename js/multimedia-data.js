eab3d7eb9ac9259ba1e2cb109512c9fd0501a1dd
Wan Hasbullah <wanhasbullahwanhussin97@gmail.com>
2026-09-01 22:36:36 +0800

    Update multimedia-data.js

--- js/multimedia-data.js
+++ js/multimedia-data.js
@@ -22,8 +22,8 @@ const VIDEO_DATA = [
   {
     id: "video-001",
     category: "ilmu-kewangan",
-    title: "Ciri-Ciri Emas Paling Untung",
-    videoSrc: "https://raw.githubusercontent.com/Wan-Hasbullah/ilmu-emas/main/video/ciri-ciri-emas-paling-untung.mp4",
+    title: "Emas Sorokkan Kekayaan",
+    videoSrc: "https://raw.githubusercontent.com/Wan-Hasbullah/ilmu-emas/main/multimedia/emas-sorokkan-kekayaan.mp4",
     poster: "https://cdn.contohdomain.com/thumbnails/motivasi-mindset-01.jpg"
   },
   {
