a7a4d7024f250c2e50d25b261dcf4e72f221cb94
Wan Hasbullah <wanhasbullahwanhussin97@gmail.com>
2026-09-26 13:21:07 +0800

    Update tipsemas-data.js

--- js/tipsemas-data.js
+++ js/tipsemas-data.js
@@ -9,31 +9,13 @@ const ARTIKEL_DATA = [
     path: "/kandungan-tipsemas/emas-tak-pernah-murah.html"
   }, // <-- Ditambah koma di sini untuk memisahkan objek pertama dan kedua
   
-  {
-    id: "sejarah-harga-emas-10-tahun",
-    title: "Sejarah Harga Emas 10 Tahun",
-    author: "Ainul Haya",
-    category: "Sejarah Harga Emas",
-    summary: "Paras harga emas semasa yang sedang rendah merupakan peluang terbaik untuk pelabur menambah simpanan, bukannya untuk berasa takut atau berhenti menyimpan.",
-    path: "/kandungan-artikel/sejarah-harga-emas-10-tahun.html"
-  },
-
-  {
-    id: "gap-auto-debit-kaedah-terbaik-menabung-secara-konsisten",
-    title: "GAP Auto-Debit, Kaedah Terbaik Menabung Secara Konsisten",
-    author: "Mohd Zulkifli Shafie",
-    category: "Simpanan Emas",
-    summary: "GAP Auto-Debit membantu kita menabung emas secara konsisten setiap bulan tanpa perlu buat keputusan berulang, sekali gus mempercepatkan proses menjadi kaya dengan emas.",
-    path: "/kandungan-artikel/gap-auto-debit-kaedah-terbaik-menabung-secara-konsisten.html"
-  },
-      
     {
-    id: "inilah-kisah-membuatkan-saya-simpan-emas",
-    title: "Inilah Kisah Membuatkan Saya Simpan Emas",
+    id: "kesilapan-pertama-membeli-emas",
+    title: "Kesilapan Pertama Membeli Emas",
     author: "Mohd Zulkifli Shafie",
-    category: "Buku Wang Emas",
-    summary: "Berapa pun harganya, tetap orang akan kata ianya \u201cmahal\u201d. Ia memang barang yang dianggap mahal sejak zaman berzaman.",
-    path: "/kandungan-artikel/inilah-kisah-membuatkan-saya-simpan-emas.html"
+    category: "Bab 1 : Emas & Kewangan",
+    summary: "kesilapan pertama bila nak beli emas ialah - tunggu harga emas murah.",
+    path: "/kandungan-tipsemas/kesilapan-pertama-membeli-emas.html"
   },
   
   
